61b1c2a1e2bf936b9a2473c11c3f3b20d4602518
drunchevski <drunchevski@gmail.com>
2021-06-12 22:22:32 +0300

    creatures

--- tasks/creatures.js
+++ tasks/creatures.js
@@ -33,56 +33,24 @@ const specieByStatusStats = undefined;
 
 // Вернуть аттрибуты заданного существа. ('NOT_FOUND' в случае отсуствия информации)
 //  getAttributes(db, 'orleon') ->  { STR: 8, INT: 2, ..., DEF: 2 }
-const getAttributes = (db, specie) => db[specie]?.attributes ?? "NOT_FOUND";
-// console.log(getAttributes(db, "everlio"));
-// console.log(getAttributes(db, 0));
-// console.log(getAttributes(db, "furry"));
-
+const getAttributes = undefined;
 // Вернуть более сильное из двух существ по заданному атрибуту ('NOT_FOUND' в случае отсуствия информации)
 // Вернуть 'DRAW' если равны
 // compareCreatures(db, orleon, fury, STR) -> fury;
-const compareCreatures = (db, creatureA, creatureB, attr) => {
-  const attrsA = getAttributes(db, creatureA);
-  const attrsB = getAttributes(db, creatureB);
-  const a = attrsA !== "NOT_FOUND" ? attrsA[attr] : undefined;
-  const b = attrsB !== "NOT_FOUND" ? attrsB[attr] : undefined;
-  if (a === undefined || b === undefined) return "NOT_FOUND";
-
-  if (a === b) return "DRAW";
-  if (a > b) return creatureA;
-  return creatureB;
-};
-// console.log(compareCreatures(db, "orleon", "fury", "STR"));
-// console.log(compareCreatures(db, "orleon", "fury", "AGL"));
-// console.log(compareCreatures(db, "orleon", "furry", "AGL"));
-// console.log(compareCreatures(db, "orlean", "fury", "AGL"));
-// console.log(compareCreatures(db, "orleon", "fury", "TUP"));
-// console.log(compareCreatures(db, "orlean", "furry", "TUP"));
-// console.log(compareCreatures(db, "orlean", "furry", "STR"));
-// console.log(compareCreatures(db, "buldiga", "fury", "INT"));
-// console.log(compareCreatures(db, "saprano", "everlio", "DEF"));
+const compareCreatures = undefined;
 
 // Для заданного существа вернуть массив из всех, кто будет им "побежден" в бою
 // Формула урона: ATK + значение его максималного атрибута из числа STR INT и AGL
 // Формула защиты: DEF + также значение его максимального атрибута
 // Итоговая формула победы: ATK - DEF (Ничью считать проигрышем)
 // fightAll(db, 'orleon') -> ['buldiga', 'everlio', 'saprano'];
+const fightAll = undefined;
 
-const fightAll = (db, specie) => {
-  const attackerAttrs = getAttributes(db, specie);
-  const getMaxAttr = ({ STR, INT, AGL }) => Math.max(STR, INT, AGL);
-  const attack = attackerAttrs.ATK + getMaxAttr(attackerAttrs);
-
-  const results = [];
-  for (const defender in db) {
-    if (defender === specie) continue;
-    const defenderAttrs = getAttributes(db, defender);
-    const defense = defenderAttrs.DEF + getMaxAttr(defenderAttrs);
-    if (attack - defense > 0) results.push(defender);
-  }
-  return results;
+module.exports = {
+  harmedBySpecie,
+  statsByStatus,
+  specieByStatusStats,
+  getAttributes,
+  compareCreatures,
+  fightAll,
 };
-
-console.log(fightAll(db, "orleon"));
-
-module.exports = { harmedBySpecie, statsByStatus, specieByStatusStats };
--- tests/creatures.test.tsx
+++ tests/creatures.test.tsx
@@ -3,6 +3,9 @@ import {
   harmedBySpecie,
   statsByStatus,
   specieByStatusStats,
+  getAttributes,
+  compareCreatures,
+  fightAll,
 } from "../tasks/creatures";
 import { STATUSES } from "../data/creatures_additional_info.json";
 
@@ -49,7 +52,52 @@ const mySpecieByStatusStats = (db: CREATURE_DB) => {
   return resultTable;
 };
 
+const myGetAttributes = (db: CREATURE_DB, specie: string) =>
+  db[specie]?.attributes ?? "NOT_FOUND";
+
+const myCompareCreatures = (
+  db: CREATURE_DB,
+  creatureA: string,
+  creatureB: string,
+  attr: ATRS | any
+) => {
+  const attrsA = myGetAttributes(db, creatureA);
+  const attrsB = myGetAttributes(db, creatureB);
+  const a = attrsA !== "NOT_FOUND" ? attrsA[attr] : undefined;
+  const b = attrsB !== "NOT_FOUND" ? attrsB[attr] : undefined;
+  if (a === undefined || b === undefined) return "NOT_FOUND";
+
+  if (a === b) return "DRAW";
+  if (a > b) return creatureA;
+  return creatureB;
+};
+
+const myFightAll = (db: CREATURE_DB, specie: string) => {
+  const attackerAttrs = myGetAttributes(db, specie);
+  if (attackerAttrs == "NOT_FOUND") throw { attackerAttrs, specie };
+  const getMaxAttr = ({ STR, INT, AGL }) => Math.max(STR, INT, AGL);
+  const attack = attackerAttrs.ATK + getMaxAttr(attackerAttrs);
+
+  const results = [];
+  for (const defender in db) {
+    if (defender === specie) continue;
+    const defenderAttrs = myGetAttributes(db, defender);
+    if (defenderAttrs === "NOT_FOUND") throw { defenderAttrs, defender };
+    const defense = defenderAttrs.DEF + getMaxAttr(defenderAttrs);
+    if (attack - defense > 0) results.push(defender);
+  }
+  return results;
+};
+
 describe("CREATURES TASKS", () => {
+  afterEach(() => {
+    console.log(
+      "\x1b[36m",
+      `${expect.getState().currentTestName}: PASSED`,
+      "\x1b[0m"
+    );
+  });
+
   (harmedBySpecie !== undefined ? test : test.skip)(
     "Testing myHarmedBySpecie",
     () => {
@@ -76,4 +124,59 @@ describe("CREATURES TASKS", () => {
       );
     }
   );
+
+  (getAttributes !== undefined ? test : test.skip)(
+    "Testing getAttributes",
+    () => {
+      expect(getAttributes(creatures_db, "orleon")).toEqual(
+        myGetAttributes(creatures_db, "orleon")
+      );
+      expect(getAttributes(creatures_db, "wrong_specie")).toEqual(
+        myGetAttributes(creatures_db, "wrong_specie")
+      );
+    }
+  );
+
+  (compareCreatures !== undefined ? test : test.skip)(
+    "Testing compareCreatures",
+    () => {
+      expect(compareCreatures(creatures_db, "orleon", "fury", "STR")).toEqual(
+        myCompareCreatures(creatures_db, "orleon", "fury", "STR")
+      );
+      expect(compareCreatures(creatures_db, "orleon", "fury", "AGL")).toEqual(
+        myCompareCreatures(creatures_db, "orleon", "fury", "AGL")
+      );
+      expect(compareCreatures(creatures_db, "orleon", "furry", "AGL")).toEqual(
+        myCompareCreatures(creatures_db, "orleon", "furry", "AGL")
+      );
+      expect(compareCreatures(creatures_db, "orlean", "fury", "AGL")).toEqual(
+        myCompareCreatures(creatures_db, "orlean", "fury", "AGL")
+      );
+      expect(compareCreatures(creatures_db, "orleon", "fury", "TUP")).toEqual(
+        myCompareCreatures(creatures_db, "orleon", "fury", "TUP")
+      );
+      expect(compareCreatures(creatures_db, "orlean", "furry", "TUP")).toEqual(
+        myCompareCreatures(creatures_db, "orlean", "furry", "TUP")
+      );
+      expect(compareCreatures(creatures_db, "orlean", "furry", "STR")).toEqual(
+        myCompareCreatures(creatures_db, "orlean", "furry", "STR")
+      );
+      expect(compareCreatures(creatures_db, "buldiga", "fury", "INT")).toEqual(
+        myCompareCreatures(creatures_db, "buldiga", "fury", "INT")
+      );
+      expect(
+        compareCreatures(creatures_db, "saprano", "everlio", "DEF")
+      ).toEqual(myCompareCreatures(creatures_db, "saprano", "everlio", "DEF"));
+    }
+  );
+
+  (fightAll !== undefined ? test : test.skip)("Testing fightAll", () => {
+    expect(fightAll(creatures_db, "orleon")).toEqual(
+      myFightAll(creatures_db, "orleon")
+    );
+  });
+
+  // console.log(getAttributes(db, "everlio"));
+  // console.log(getAttributes(db, 0));
+  // console.log(getAttributes(db, "furry"));
 });
